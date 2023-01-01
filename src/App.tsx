3804e555e9a9c998c48091ca29cffb5deba851a1
duyonix <dtduy2210@gmail.com>
2023-01-01 14:27:52 +0700

    update loading and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useUnityContext } from 'react-unity-webgl';
-import { Unity } from 'react-unity-webgl/distribution/components/unity-component';
-import './App.css';
+import React, { Fragment } from "react";
+import { useUnityContext } from "react-unity-webgl";
+import { Unity } from "react-unity-webgl/distribution/components/unity-component";
+import "./App.css";
 
 const App = () => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: "build/unity-build-webgl.loader.js",
     dataUrl: "build/unity-build-webgl.data",
     frameworkUrl: "build/unity-build-webgl.framework.js",
@@ -12,15 +12,54 @@ const App = () => {
   });
 
   const elementStyles = {
-    height: '100vh',
+    height: "100vh",
     width: `${(16 / 9) * 100}vh`,
   };
 
   return (
-    <div>
-      <Unity style={elementStyles} unityProvider={unityProvider} />
-    </div>
+    <Fragment>
+      {!isLoaded && (
+        <div className="wrapper-loading">
+          <div className="boxes">
+            <div className="box">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="box">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="box">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="box">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="wrapper">
+        <Unity
+          unityProvider={unityProvider}
+          style={{
+            visibility: isLoaded ? "visible" : "hidden",
+            ...elementStyles,
+          }}
+        />
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;

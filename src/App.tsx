9591a9f4edf407eaeaf68065152abdc243dd9f8d
duyonix <dtduy2210@gmail.com>
2023-01-01 12:54:35 +0700

    build webgl

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useUnityContext } from 'react-unity-webgl';
+import { Unity } from 'react-unity-webgl/distribution/components/unity-component';
 import './App.css';
 
-function App() {
+const App = () => {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/unity-build-webgl.loader.js",
+    dataUrl: "build/unity-build-webgl.data",
+    frameworkUrl: "build/unity-build-webgl.framework.js",
+    codeUrl: "build/unity-build-webgl.wasm",
+  });
+
+  const elementStyles = {
+    height: '100vh',
+    width: `${(16 / 9) * 100}vh`,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Unity style={elementStyles} unityProvider={unityProvider} />
     </div>
   );
 }
